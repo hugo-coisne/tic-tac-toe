bb66e9bc819e1fa1d812096b78e90ce6defabd82
hugo_coisne <coisnehugo@gmail.com>
2024-01-21 19:10:15 +0100

    create player factory

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
 const gameboard = (function () {
-    const game = [['', '', ''], ['', '', ''], ['', '', '']]
-    const logGame = function(){
-        console.log(game)
+  const game = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+  const logGame = function () {
+    console.log(game);
+  };
+  const place = function (char, x, y) {
+    if (game[y][x] == "") {
+      game[y][x] = char;
+    } else {
+      throw new Error(`The (${x},${y}) slot already is taken.`);
     }
-    const place = function (char, x, y) {
-        if (game[y][x] == '') {
-            game[y][x] = char
-        } else {
-            throw new Error(`The (${x},${y}) slot already is taken.`)
-        }
-    }
+  };
 
-    return {logGame, place}
+  return { logGame, place };
 })();
 
-gameboard.logGame()
-gameboard.place('x', 1, 0)
-gameboard.logGame()
+const createPlayer = function (char) {
+  const score = 0;
+  const char = char;
+  const getScore = function () {
+    return score;
+  };
+  const place = function (x, y) {
+    gameboard.place(char, x, y);
+  };
+  return { getScore, place };
+};
+
+gameboard.logGame();
+gameboard.place("x", 1, 0);
+gameboard.logGame();
